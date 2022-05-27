73a5c448d1c2385b078b5d1a1366d7479ce3f64a
GuilhermeBonni <bonni.guilherme@gmail.com>
2022-05-27 10:43:28 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,11 +1,18 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
-import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
@@ -66,18 +73,26 @@ public class User implements Serializable {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;	
+		int result = 1;	
+		result = prime * result + ((id == null) ? 0 : id.hashCode());	
+		return result;
 	}
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		User other = (User) obj;
-		return Objects.equals(id, other.id);
+	@Override	
+	public boolean equals(Object obj) {	
+		if (this == obj)	
+			return true;	
+		if (obj == null)	
+			return false;	
+		if (getClass() != obj.getClass())	
+			return false;	
+		User other = (User) obj;	
+		if (id == null) {	
+			if (other.id != null)	
+				return false;	
+		} else if (!id.equals(other.id))	
+			return false;	
+		return true;	
 	}
 }
